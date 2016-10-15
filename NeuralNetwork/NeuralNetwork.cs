407b19b3b115a050287440a60d0f68bedcc82adf
DimaBolsunov <Dima.Bolsunov@gmail.com>
2016-10-15 14:51:45 +0300

    first commit

---
+++ NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class NeuralNetwork
+    {
+        public double learnRate = 0.2;
+        public double[][] layers; //PRIVATE!!!
+        public double[][,] weights; //MAKE PRIVATE
+        public NeuralNetwork(int inputs,int outputs, int[] LayersSize = null)
+        {
+            //this.inputs = new double[inputs];
+            //this.outputs = new double[outputs];
+            
+            layers = new double[LayersSize.Length+2][];
+            layers[0] = new double[inputs];
+            if (LayersSize != null)
+            {
+                for (int i = 0; i < LayersSize.Length; i++)
+                {
+                    layers[i+1] = new double[LayersSize[i]];
+                }
+            }
+            layers[LayersSize.Length + 1] = new double[outputs];
+            InitializeWeights();
+        }
+        private void InitializeWeights()
+        {
+            weights = new double[layers.Length - 1][,];
+            for (int i = 0; i < layers.Length - 1; i++)
+            {
+                weights[i] = new double[layers[i].Length + 1, layers[i + 1].Length];
+                for (int j = 0; j < layers[i].Length + 1; j++)
+                {
+                    for (int k = 0; k < layers[i + 1].Length; k++)
+                    {
+                        weights[i][j, k] = new Random().NextDouble()-0.5d;
+                    }
+                }
+            }
+        }
+        private void checkValues(double[] inps, double[] outps)
+        {
+            if (inps.Length != layers[0].Length || outps.Length != layers[layers.Length - 1].Length) throw new Exception("Wrong data format!");
+            }
+        public void train(double[] inps, double[] outps)
+        {
+            checkValues(inps, outps);
+            double[] results = getResult(inps);
+            double[][] err = new double[layers.Length][];
+            err[layers.Length - 1] = new double[layers[layers.Length - 1].Length];
+            for (int o = 0; o < layers[layers.Length - 1].Length; o++) //Ошибка выходного слоя
+            {
+                err[layers.Length - 1][o] = (outps[o] - layers[layers.Length - 1][o]) * sigmoidDerivative(layers[layers.Length - 1][o]);
+            }
+            for (int layer = layers.Length - 1; layer > 0; layer--)
+            {
+                err[layer-1] = new double[layers[layer-1].Length];
+                for (int cur = 0; cur < layers[layer - 1].Length; cur++)
+                {
+                    err[layer-1][cur] = 0;
+                    for (int next = 0; next < layers[layer].Length; next++)
+                    {
+                        err[layer - 1][cur] += err[layer][next];
+                    }
+                    err[layer-1][cur] *= sigmoidDerivative(layers[layer-1][cur]);
+                }
+                for (int cur = 0; cur < layers[layer].Length; cur++)
+                {
+                    for (int prev = 0; prev < layers[layer-1].Length; prev++)
+                    {
+                        weights[layer - 1][prev, cur] += learnRate * err[layer][cur] * layers[layer - 1][prev];
+                    }
+                    weights[layer - 1][layers[layer - 1].Length, cur] += learnRate * err[layer][cur];
+                }
+            }
+        }
+        private double sigmoid(double val)
+        {
+            return (val * (1.0 + Math.Exp(-val)));
+        }
+        private double sigmoidDerivative(double val)
+        {
+            return (val * (1.0 -val));
+        }
+        public double[] getResult(double[] inps)
+        {
+            layers[0] = inps;
+            checkValues(inps, layers[layers.Length - 1]);
+            for (int layer = 1; layer < layers.Length; layer++)
+            {
+                for (int cur = 0; cur < layers[layer].Length; cur++)
+                {
+                    double sum = 0;
+                    for (int prev = 0; prev < layers[layer - 1].Length; prev++)
+                    {
+                        sum += layers[layer - 1][prev] * weights[layer-1][prev,cur];
+                    }
+                    layers[layer][cur] = sigmoid(sum);
+                }
+            }
+
+            return layers[layers.Length - 1];
+            //return new double[]{};
+        }
+        public override string ToString()
+        {
+            string a = "";
+            for (int i = 0; i < layers.Length; i++) a += layers[i].Length.ToString()+":";
+            a = a.Remove(a.Length-1);
+            return  a;
+        }
+    }
+}
---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class Program
+    {
+        static List<double> results = new List<double>();
+
+
+        static void Main(string[] args)
+        {
+            NeuralNetwork net = new NeuralNetwork(2,1,new int[] {2});
+            Console.WriteLine(net);
+            for (int i = 0; i < net.weights.Length; i++)
+            {
+                Console.WriteLine(net.weights[i].ToString());
+            }
+            getResults(net);
+            for (int i = 0; i < 50; i++)
+            {
+                train(net);
+            }
+            getResults(net);
+
+            Console.ReadLine();
+        }
+        static void train(NeuralNetwork net)
+        {
+            net.train(new double[] { 0, 0 }, new double[] { 0 });
+            net.train(new double[] { 0, 1 }, new double[] { 1 });
+            net.train(new double[] { 1, 0 }, new double[] { 1 });
+            net.train(new double[] { 1, 1 }, new double[] { 0 });
+        }
+
+        static void getResults(NeuralNetwork net)
+        {
+            results = new List<double>();
+            results.Add(net.getResult(new double[] { 0, 0 })[0]);
+            results.Add(net.getResult(new double[] { 0, 1 })[0]);
+            results.Add(net.getResult(new double[] { 1, 0 })[0]);
+            results.Add(net.getResult(new double[] { 1, 1 })[0]);
+            showResults();
+        }
+
+        static void showResults()
+        {
+            Console.WriteLine("Showing results {");
+            foreach (double d in results)
+            {
+                Console.WriteLine(d);
+            }
+            Console.WriteLine("}");
+        }
+    }
+}
